6f1d6451e135b537fce5de413a4041952959a14a
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-06 00:44:25 +0300

    [task] Added working comments service methods

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,10 +1,10 @@
+import { ideaEntity } from './../idea/idea.entity';
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { commentEntity } from './comment.entity';
 import { userEntity } from '../user/user.entity';
-import { ideaEntity } from '../../dist/idea/idea.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([commentEntity, userEntity, ideaEntity])],
@@ -12,4 +12,3 @@ import { ideaEntity } from '../../dist/idea/idea.entity';
   providers: [CommentService],
 })
 export class CommentModule {}
- 
